7ed0917e9ad3be7d274a2af3cb7117c356732eea
giselitapalacios12 <103265483+giselitapalacios12@users.noreply.github.com>
2025-05-17 07:45:48 -0500

    Update TransFiCollectible.sol

--- TransFiCollectible.sol
+++ TransFiCollectible.sol
@@ -8,11 +8,18 @@ contract TransFiCollectible is ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
+    uint256 public maxNFTsPerWallet = 5;
+
     constructor() ERC721("TransFi Collectible", "TFC") {}
 
+ // Aquí se aplica la condición
     function mintCollectible(address recipient) public returns (uint256) {
-        _tokenIds.increment();
+        require(
+            balanceOf(recipient) < maxNFTsPerWallet,
+            "Solo puedes tener hasta 5 TFC (coleccionables)"
+        );
 
+        _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
         _mint(recipient, newItemId);
 
